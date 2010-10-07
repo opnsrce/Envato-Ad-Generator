23ec53f2d86639a120e62f441de218af317a3a3b
Levi Hackwith <levi.hackwith@gmail.com>
2010-10-07 04:05:15 +0000

    main.rb     - Changed 'get_popular_items_this_week' call to 'get_popular_items_last_week'
    envato_api.rb
        - Initialize function now takes api version as optional parameter
        - Method get_data now takes an optional &block parameter
        - Cleaned up 'case format' statement to remove some code duplication
        - Added generate_url method
        - Renamed get_popular_items_this_week to get_popular_items_last_week
        - Added get_blog_posts
        - Method get_custom no longer takes XPath parameter but does take a &block instead
        - Ignoring nbproject folder

--- lib/envato_api.rb
+++ lib/envato_api.rb
@@ -1,33 +1,39 @@
 class EnvatoAPI
-  def initialize
-    @api_version = 'v2';
+  def initialize(version = 'v2')
+    @api_version = version;
     @base_url = 'http://marketplace.envato.com/api';
   end
   private
-  def get_data(url, format)
+  def get_data(url, format, &block)
     data = Net::HTTP.get_response(URI.parse(URI.encode(url))).body;
     case format
     when 'xml' then
-      return REXML::Document.new(data);
+      data = REXML::Document.new(data);
     end
+    data = yield data if block_given?
+    return data;
+  end
+  def generate_url(api_command, format, params = nil)
+    return "#{@base_url}/#{@api_version}/#{api_command}:#{params}.#{format}";
   end
   public
-  def get_popular_items_this_week(site, format)
-    url = "#{@base_url}/#{@api_version}/popular:#{site}.#{format}";
+  def get_blog_posts(site, format)
+    url = generate_url('blog-posts', format, site);
+    return get_data(url, format);
+  end
+
+  def get_popular_items_last_week(site, format)
+    url = generate_url('popular', format, site);
     case format
     when 'xml' then
-      xml_doc = get_data(url, 'xml');
-      data = REXML::XPath.match(xml_doc, '//items-last-week/');
+      data = get_data(url,format) {
+        |xml_doc| return REXML::XPath.match(xml_doc, '//items-last-week/')
+      };
     end
     return data;
   end
-  def get_custom(set, format, parameters = nil, xpath = '//')
-    url = "#{@base_url}/#{@api_version}/#{set}:#{parameters}.#{format}";
-    case format
-    when 'xml' then
-      xml_doc = self.get_data(url, 'xml');
-      data = REXML::XPath.match(xml_doc, xpath);
-    end
-    return data;
+  def get_custom(set, format, params = nil, &block)
+    url = generate_url(set, format, params);
+    return get_data(url, format, &block);
   end
 end
--- lib/main.rb
+++ lib/main.rb
@@ -9,7 +9,7 @@ require 'envato_ad_generator';
 
 api = EnvatoAPI.new
 thumbnails = Array.new
-popular_items = api.get_popular_items_this_week('graphicriver', 'xml');
+popular_items = api.get_popular_items_last_week('graphicriver', 'xml');
 REXML::XPath.match(popular_items, '//thumbnail').each do |thumbnail|
    thumbnails << thumbnail.text;
 end
